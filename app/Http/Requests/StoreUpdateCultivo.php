972af64669776d1c5aff9a6bc6b77f66f3417a1e
guidoolliveira <guidovidal9@gmail.com>
2025-04-13 23:13:16 -0300

    Validação da tabela de cultivos

---
+++ app/Http/Requests/StoreUpdateCultivo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateCultivo extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'viveiro_id' => ['required', 'exists:viveiros,id'],
+            'data_inicio' => ['required', 'date'],
+            'quantidade_camarao' => ['required', 'numeric'],
+            'data_fim' => ['nullable', 'date', 'after_or_equal:data_inicio'],
+        ];
+    }
+}
